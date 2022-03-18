07951be60145e30ca6a4079a6d71e527fd461100
Guilherme Tinen Ortolani <guilherme.tinen@cantacom.com.br>
2022-03-18 17:24:55 -0300

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function index(Request $request) {
+
+    }
+
+    public function store(Request $request) {
+
+    }
+
+    //
+}
